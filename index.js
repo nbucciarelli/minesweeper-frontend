f8ac1170e356a00bb4bfb7abeb722e1831552662
Nicholas Bucciarelli <nbucciarelli@Nicholass-MBP.attlocal.net>
2016-05-30 01:57:15 -0400

    Fixed issue with array.

--- index.js
+++ index.js
@@ -1,61 +1,71 @@
 var _ = require('underscore');
-var prompt = require('prompt');
+// var prompt = require('prompt');
+//
+// prompt.start();
+//
+// console.log('What size board would you like?');
+// prompt.get(['Board Width', 'Board Height', 'Mine Count'], function (err, result) {
+//   if (err) { return onErr(err); }
+//   // console.log(result);
+//   // console.log('  Width: ' + result['Board Width']);
+//   // console.log('  Height: ' + result['Board Height']);
+//   startGame(result['Board Width'], result['Board Height'], result['Mine Count']);
+// });
+//
+// function onErr(err) {
+//   console.log(err);
+//   return 1;
+// }
 
-prompt.start();
-
-console.log('What size board would you like?');
-prompt.get(['Board Width', 'Board Height', 'Mine Count'], function (err, result) {
-  if (err) { return onErr(err); }
-  // console.log(result);
-  // console.log('  Width: ' + result['Board Width']);
-  // console.log('  Height: ' + result['Board Height']);
-  startGame(result['Board Width'], result['Board Height'], result['Mine Count']);
-});
-
-function onErr(err) {
-  console.log(err);
-  return 1;
-}
+var board
 
 function startGame(width, height, mineCount) {
-  var dood = createBoard(width, height, 0);
-  setMines(dood, mineCount);
+  var gameBoard = setMines(createBoard(width, height, 0), mineCount);
+
 }
 
 function createBoard(width, height, val) {
-  var widthArr = [];
-  var arr = [];
-
-  _.times(width, function(x) { widthArr.push(val); })
-  _.times(height, function(w) { arr.push(widthArr); })
+  var arr = new Array();
+  _.times(height, function(h) {
+    var widthArr = new Array();
+    _.times(width, function(w) {
+      widthArr.push(val);
+    });
+    arr.push(widthArr);
+  })
   return arr;
 }
 
 function setMines(board, mineCount) {
   console.log(board);
+  _.each(board, function(row) {
+    _.each(row, function(cell){
+      console.log(cell);
+    })
+  })
   var height = board.length;
   var width = board[0].length;
   // I'm not sure why it won't let me change 2d array variables
   // var flattenedBoard = _.flatten(board)
   _.times(mineCount, function(mine) {
-    // console.log(mine);
     var minePosX = parseInt(Math.random(height) * height);
     var minePosY = parseInt(Math.random(width) * width);
-    console.log("X: ", minePosX, " Y: ", minePosY);
-    // console.log(flattenedBoard);
-    // console.log("Position: ", (minePosY * minePosY) - 1);
-    // flattenedBoard[(minePosY * minePosY) - 1] = 1;
-
-    // This won't work for some reason...
-    // console.log(board[minePosY][minePosX]);
+    while(board[minePosY][minePosX] == 1) {
+      minePosX = parseInt(Math.random(height) * height);
+      minePosY = parseInt(Math.random(width) * width);
+      if(board[minePosY][minePosX] === 0) {
+        console.log("break");
+        break;
+      }
+    }
     board[minePosY][minePosX] = 1;
   });
-
-  // _.times(height, function(x) {
-  //   _.times(width, function(y) {
-  //     board[x][y] = flattenedBoard[(y*height) + x];
-  //   });
-  // });
   console.log(board);
+  _.each(board, function(row) {
+    _.each(row, function(cell){
+      console.log(cell);
+    })
+  })
+
   return board;
 }
